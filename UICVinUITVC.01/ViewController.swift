aff4117376ffc88946fb6bbf755a3fa711ff0ff6
Tifozoso <mfranks@gmail.com>
2018-01-12 15:38:39 -0800

    Initial Commit

---
+++ UICVinUITVC.01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UICVinUITVC.01
+//
+//  Created by Michael Franks on 1/12/18.
+//  Copyright © 2018 Michael Franks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
